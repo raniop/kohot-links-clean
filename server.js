7e348915854b03a98a06fefd5dd8a600ca4d6132
Rani Ophir <raniop@mac.com>
2025-07-19 22:00:20 +0300

    🔧 תיקון content-type של apple-app-site-association

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+  res.setHeader('Content-Type', 'application/json'); // כפייה לתגובה תקינה
+  res.sendFile(path.join(__dirname, 'public', '.well-known', 'apple-app-site-association'));
+});
 
 // משרת את כל הקבצים שבתוך public כולל .well-known
 app.use(express.static(path.join(__dirname, 'public')));
